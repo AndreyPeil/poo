c7a1efe2b56cca00447b757a801511cf79aa80e9
AndreyPeil <126731474+AndreyPeil@users.noreply.github.com>
2024-08-24 19:16:40 -0300

    Create veiculo.java

---
+++ carros/veiculo.java
@@ -0,0 +1,15 @@
+public class veiculo{
+    String marca = "Mazda";
+    String modelo = "Miata";
+    int ano = 1995;
+    motor motor;
+
+
+    public static void ligado() {
+        System.out.println("o carro está ligado");
+    }   
+
+    public static void desligado() {
+        System.out.println("o carro está desligado");
+    }
+}   
