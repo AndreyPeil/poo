41ee67eaf2c50cb26b7013b985ea7a22e7b2d54e
AndreyPeil <126731474+AndreyPeil@users.noreply.github.com>
2024-10-27 10:48:24 -0300

    Create Main.java

---
+++ g1/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+
+//TODO all the code
+    public static void main(String[] args) {
+        System.out.println("Eletrocats, your electric car rental service");
+        System.out.println("Please select what you want to do:");
+        System.out.println("1. Add a new car");
+
+    }
+}
