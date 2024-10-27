6f9e67babec847cefa39fcc1c3baa8888ae0481e
AndreyPeil <126731474+AndreyPeil@users.noreply.github.com>
2024-10-27 10:50:03 -0300

    Create Sedan.java

---
+++ g1/Sedan.java
@@ -0,0 +1,9 @@
+//same as the compact, just reference the UBERKLASS here lol
+
+public class Sedan extends Default_car_specs{
+    protected String chargin_time_sedan = "6 hours";
+    public Sedan(String car_brand, String car_model, String car_fabrication_year, String car_battery_capacity, String chargin_time_sedan){
+        super(car_brand, car_model, car_fabrication_year, car_battery_capacity, "300");
+        this.chargin_time_sedan = chargin_time_sedan;
+    }
+}  
