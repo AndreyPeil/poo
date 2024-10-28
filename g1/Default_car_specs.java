d5c91b8ec9cab3b385d3be3cbe1c702654b01cc2
Otávio Ferreira <126731189+otaviof3@users.noreply.github.com>
2024-10-28 15:27:33 -0300

    Update Default_car_specs.java
    adicionei a variável do Identificador único e mudei os tipos das variáveis.

--- g1/Default_car_specs.java
+++ g1/Default_car_specs.java
@@ -1,18 +1,24 @@
-//here is a bunch of information about the cars, I'll be using heritance cuz lazy LOL
+// Here is a bunch of information about the cars, I'll be using inheritance 
 
 public class Default_car_specs{
+    protected int identifier;
     protected String car_brand;
     protected String car_model;
-    protected String car_fabrication_year;
-    protected String car_battery_capacity;
-    protected String car_max_range;
+    protected int car_fabrication_year;
+    protected double car_battery_capacity;
+    protected double car_max_range;
 
-    public Default_car_specs(String car_brand, String car_model, String car_fabrication_year, String car_battery_capacity, String car_max_range){
+    public Default_car_specs(int identifier, String car_brand, String car_model, int car_fabrication_year, double car_battery_capacity, double car_max_range){
+        this.identifier = identifier;
         this.car_brand = car_brand;
         this.car_model = car_model;
         this.car_fabrication_year = car_fabrication_year;
         this.car_battery_capacity = car_battery_capacity;
         this.car_max_range = car_max_range;
+    }
+
+    public int getIdentifier(){
+        return identifier;
     }
 
     public String getCar_brand(){
@@ -23,16 +29,20 @@ public class Default_car_specs{
         return car_model;
     }
 
-    public String getCar_fabrication_year(){
+    public int getCar_fabrication_year(){
         return car_fabrication_year;
     }
 
-    public String getCar_battery_capacity(){
+    public double getCar_battery_capacity(){
         return car_battery_capacity;
     }
 
-    public String getCar_max_range(){
+    public double getCar_max_range(){
         return car_max_range;
+    }
+
+    public void setIdentifier(int identifier){
+        this.identifier = identifier;
     }
 
     public void setCar_brand(String car_brand){
@@ -43,22 +53,21 @@ public class Default_car_specs{
         this.car_model = car_model;
     }
 
-    public void setCar_fabrication_year(String car_fabrication_year){
+    public void setCar_fabrication_year(int car_fabrication_year){
         this.car_fabrication_year = car_fabrication_year;
     }
 
-    public void setCar_battery_capacity(String car_battery_capacity){
+    public void setCar_battery_capacity(double car_battery_capacity){
         this.car_battery_capacity = car_battery_capacity;
     }
 
-    public void setCar_max_range(String car_max_range){
+    public void setCar_max_range(double car_max_range){
         this.car_max_range = car_max_range;
     }
 
     @Override
     public String toString(){
-        return "Car brand: " + car_brand + "\nCar model: " + car_model + "\nCar fabrication year: " + car_fabrication_year + "\nCar battery capacity: " + car_battery_capacity + "\nCar max range: " + car_max_range;
+        return "Car ID: " + identifier + "\nCar brand: " + car_brand + "\nCar model: " + car_model + "\nCar fabrication year: " + car_fabrication_year + "\nCar battery capacity: " + car_battery_capacity + "\nCar max range: " + car_max_range;
     }
-
 
 }
