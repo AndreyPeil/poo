7644efba7b9795b41dc62ac405622ba3382a5e84
Otávio Ferreira <126731189+otaviof3@users.noreply.github.com>
2024-10-30 16:06:41 -0300

    Update Chargin_station.java
    Cassol na aula 1 de Programação Orientada a Objetos:
    - "Nome da classe e do arquivo ".java" devem ser iguais."
    
    Andrey:

--- g1/Chargin_station.java
+++ g1/Chargin_station.java
@@ -1,55 +1,54 @@
-//this one is a MUST
-//here is the class for our MVP, the charging station
-//This dude os OP, he can charge any car, but he has a limit of cars to charge at the same time
-//kinda like a playstation, but for charging your car
+//This one is extremely important for the functioning of electrical cars, the Charging Station class.
+//Every station can charge any car type from the system, but they all also have a limit of cars they can charge at the same time.
 
-public class Charging_station{
-   protected int station_id;
+public class Chargin_station {
+
+    protected int station_id;
     protected String station_location;
     protected int station_capacity;
     protected double station_charging_time;
 
-    public Charging_station(int station_id, String station_location, int station_capacity, double station_charging_time){
+    public Chargin_station(int station_id, String station_location, int station_capacity, double station_charging_time) {
         this.station_id = station_id;
         this.station_location = station_location;
         this.station_capacity = station_capacity;
         this.station_charging_time = station_charging_time;
     }
 
-    public int getStation_id(){
+    public int getStation_id() {
         return station_id;
     }
 
-    public String getStation_location(){
+    public String getStation_location() {
         return station_location;
     }
 
-    public int getStation_capacity(){
+    public int getStation_capacity() {
         return station_capacity;
     }
 
-    public double getStation_charging_time(){
+    public double getStation_charging_time() {
         return station_charging_time;
     }
 
-    public void setStation_id(int station_id){
+    public void setStation_id(int station_id) {
         this.station_id = station_id;
     }
 
-    public void setStation_location(String station_location){
+    public void setStation_location(String station_location) {
         this.station_location = station_location;
     }
 
-    public void setStation_capacity(int station_capacity){
+    public void setStation_capacity(int station_capacity) {
         this.station_capacity = station_capacity;
     }
 
-    public void setStation_charging_time(double station_charging_time){
+    public void setStation_charging_time(double station_charging_time) {
         this.station_charging_time = station_charging_time;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "Station ID: " + station_id + "\nStation location: " + station_location + "\nStation capacity: " + station_capacity + "\nStation charging time: " + station_charging_time;
     }
 }
