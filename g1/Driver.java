c7709a57433e97ed6f8651aeb98cf1f71f71d041
AndreyPeil <126731474+AndreyPeil@users.noreply.github.com>
2024-10-29 22:38:00 -0300

    Update Driver.java

--- g1/Driver.java
+++ g1/Driver.java
@@ -1,62 +1,54 @@
-//the fleet is just 3 arraylists for all the classes
-//add, remove, print
-//do whaterver
+//here is the class for the driver, name, id, license and experience are the attributes
+//experience gonna be a b*tch do make, cuz is three possible outcomes: starter, mid and advanced
+//shit's gonna be a bunch of if else statements lol, gotta do what gotta do some times 
 
-import java.util.*;
+public class Driver{
+    protected String driver_name;
+    protected int driver_id;
+    protected String driver_license;
+    protected String driver_experience; //pay attention to this one
 
-public class Fleet {
-    private List<Default_car_specs> cars;
-    private List<Driver> drivers;
-    private List<Charging_station> stations;
-
-    public Fleet() {
-        cars = new ArrayList<>();
-        drivers = new ArrayList<>();
-        stations = new ArrayList<>();
+    public Driver(String driver_experience, int driver_id, String driver_license, String driver_name) {
+        this.driver_experience = driver_experience;
+        this.driver_id = driver_id;
+        this.driver_license = driver_license;
+        this.driver_name = driver_name;
     }
 
-    public void addCar(Default_car_specs car) {
-        cars.add(car);
+    public String getDriver_name() {
+        return driver_name;
     }
 
-    public void removeCar(Default_car_specs car) {
-        cars.remove(car);
+    public int getDriver_id() {
+        return driver_id;
     }
 
-    public void printFleet() {
-        System.out.println("Fleet of Cars:");
-        for (Default_car_specs car : cars) {
-            System.out.println(car);
-        }
+    public String getDriver_license() {
+        return driver_license;
     }
 
-    public void addDriver(Driver driver) {
-        drivers.add(driver);
+    public String getDriver_experience() {
+        return driver_experience;
     }
 
-    public void removeDriver(Driver driver) {
-        drivers.remove(driver);
+    public void setDriver_name(String driver_name) {
+        this.driver_name = driver_name;
     }
 
-    public void printDrivers() {
-        System.out.println("Drivers:");
-        for (Driver driver : drivers) {
-            System.out.println(driver);
-        }
+    public void setDriver_id(int driver_id) {
+        this.driver_id = driver_id;
     }
 
-    public void addChargingStation(Charging_station station) {
-        stations.add(station);
+    public void setDriver_license(String driver_license) {
+        this.driver_license = driver_license;
     }
 
-    public void removeChargingStation(Charging_station station) {
-        stations.remove(station);
+    public void setDriver_experience(String driver_experience) {
+        this.driver_experience = driver_experience;
     }
 
-    public void printChargingStations() {
-        System.out.println("Charging Stations:");
-        for (Charging_station station : stations) {
-            System.out.println(station);
-        }
+    @Override   
+    public String toString(){
+        return "Driver name: " + driver_name + "\nDriver ID: " + driver_id + "\nDriver license: " + driver_license + "\nDriver experience: " + driver_experience;
     }
 }
