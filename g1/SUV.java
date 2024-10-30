6439cbf4203e280ee457b08f4eb5df45981f10c0
Otávio Ferreira <126731189+otaviof3@users.noreply.github.com>
2024-10-30 15:22:03 -0300

    Update SUV.java
    Melhorias gerais.
    Tempo médio de carga atualizado.
    Autonomia máxima é presumida como 530.

--- g1/SUV.java
+++ g1/SUV.java
@@ -1,8 +1,20 @@
-//you've got the drill on this one lil bro
-public class SUV extends Default_car_specs{
-    protected String chargin_time_suv = "8 hours";
-    public SUV(String car_brand, String car_model, String car_fabrication_year, String car_battery_capacity, String chargin_time_suv){
-        super(car_brand, car_model, car_fabrication_year, car_battery_capacity, "400");
-        this.chargin_time_suv = chargin_time_suv;
+//Same as Compact and Sedan classes.
+//Max range of above 400, uses 530 in this system.
+
+public class SUV extends Default_car_specs {
+
+    protected double chargin_time_suv;
+
+    public SUV(int identifier, String car_brand, String car_model, int car_fabrication_year, double car_battery_capacity) {
+        super(identifier, car_brand, car_model, car_fabrication_year, car_battery_capacity, 530);
+        this.chargin_time_suv = 8;
+    }
+
+    public double getChargin_time_suv() {
+        return chargin_time_suv;
+    }
+
+    public void setChargin_time_suv(double chargin_time_suv) {
+        this.chargin_time_suv = 8;
     }
 }
