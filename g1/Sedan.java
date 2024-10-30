127a7c3ae8d1899649a22ac1cc8013291b40b588
Otávio Ferreira <126731189+otaviof3@users.noreply.github.com>
2024-10-30 15:15:39 -0300

    Update Sedan.java
    Melhora nos comentários e na formatação.
    Tempo médio de carga atualizado.

--- g1/Sedan.java
+++ g1/Sedan.java
@@ -1,9 +1,19 @@
-//same as the compact, just reference the UBERKLASS here lol
+//Works the same as the Compact class, extending from the Default car specs class.
 
-public class Sedan extends Default_car_specs{
-    protected String chargin_time_sedan = "6 hours";
-    public Sedan(String car_brand, String car_model, String car_fabrication_year, double car_battery_capacity, String chargin_time_sedan){
-        super(car_brand, car_model, car_fabrication_year, car_battery_capacity, "300");
-        this.chargin_time_sedan = chargin_time_sedan;
+public class Sedan extends Default_car_specs {
+
+    protected double chargin_time_sedan;
+
+    public Sedan(int identifier, String car_brand, String car_model, int car_fabrication_year, double car_battery_capacity) {
+        super(identifier, car_brand, car_model, car_fabrication_year, car_battery_capacity, 400);
+        this.chargin_time_sedan = 6;
+    }
+
+    public double getChargin_time_sedan() {
+        return chargin_time_sedan;
+    }
+
+    public void setChargin_time_sedan(double chargin_time_sedan) {
+        this.chargin_time_sedan = 6;
     }
 }
