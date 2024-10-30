c6f386ad7b5fbde87dabb0f6ffc826783c1eaa39
AndreyPeil <126731474+AndreyPeil@users.noreply.github.com>
2024-10-29 22:37:45 -0300

    Create Rechargin_event.java

---
+++ g1/Rechargin_event.java
@@ -0,0 +1,34 @@
+import java.time.LocalDateTime;
+
+public class Recharge_event {
+    private LocalDateTime date;
+    private Charging_station station;
+    private double energyCharged;
+
+    public Recharge_event(LocalDateTime date, Charging_station station, double energyCharged) {
+        this.date = date;
+        this.station = station;
+        this.energyCharged = energyCharged;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public Charging_station getStation() {
+        return station;
+    }
+
+    public double getEnergyCharged() {
+        return energyCharged;
+    }
+
+    @Override
+    public String toString() {
+        return "RechargeEvent{" +
+                "date=" + date +
+                ", station=" + station.getStation_location() +
+                ", energyCharged=" + energyCharged + " kWh" +
+                '}';
+    }
+}
