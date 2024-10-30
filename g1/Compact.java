9ab084acad55c23e28d6beaa082baf644a386196
AndreyPeil <126731474+AndreyPeil@users.noreply.github.com>
2024-10-29 22:35:42 -0300

    Update Compact.java

--- g1/Compact.java
+++ g1/Compact.java
@@ -1,19 +1,10 @@
-//Compact cars, range of 200km
-//Since every thing was made in the default class, I just need to call the constructor and pass the values.
+//Shitry for compact cars, range of 200KM (WTF IS A KILOMETER RAHHHH)
+//since every thing was made in the default class, I just need to call the constructor and pass the values. GG EZY
 //Gonna make the charging time to every car, cuz is gonna be better to have different variables to work with
 public class Compact extends Default_car_specs{
-    protected double chargin_time_compact;
-
-    public Compact(int identifier, String car_brand, String car_model, int car_fabrication_year, double car_battery_capacity){
-        super(identifier, car_brand, car_model, car_fabrication_year, car_battery_capacity, 200);
-        this.chargin_time_compact = 4;
-    }
-
-    public double getChargin_time_compact(){
-        return chargin_time_compact;
-    }
-
-    public void setChargin_time_compact(double chargin_time_compact){
-        this.chargin_time_compact = 4;
+    protected String chargin_time_compact = "4 hours";
+    public Compact(String car_brand, String car_model, String car_fabrication_year, double car_battery_capacity, String chargin_time_compact){
+        super(car_brand, car_model, car_fabrication_year, car_battery_capacity, "200");
+        this.chargin_time_compact = chargin_time_compact;
     }
 }
