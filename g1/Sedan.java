1ab62542354d652e6acd4c815e1a0088b54b816a
AndreyPeil <126731474+AndreyPeil@users.noreply.github.com>
2024-10-29 22:36:05 -0300

    Update Sedan.java

--- g1/Sedan.java
+++ g1/Sedan.java
@@ -2,8 +2,8 @@
 
 public class Sedan extends Default_car_specs{
     protected String chargin_time_sedan = "6 hours";
-    public Sedan(String car_brand, String car_model, String car_fabrication_year, String car_battery_capacity, String chargin_time_sedan){
+    public Sedan(String car_brand, String car_model, String car_fabrication_year, double car_battery_capacity, String chargin_time_sedan){
         super(car_brand, car_model, car_fabrication_year, car_battery_capacity, "300");
         this.chargin_time_sedan = chargin_time_sedan;
     }
-}  
+}
