6f1276a96c76485a2f6906ca9399bf82c01e2861
Otávio Ferreira <126731189+otaviof3@users.noreply.github.com>
2024-11-03 22:23:31 -0300

    Update Default_car_specs.java

--- g1/Default_car_specs.java
+++ g1/Default_car_specs.java
@@ -24,57 +24,65 @@ public class Default_car_specs {
         return identifier;
     }
 
-    public String getCar_brand() {
-        return car_brand;
-    }
-
-    public String getCar_model() {
-        return car_model;
-    }
-
-    public int getCar_fabrication_year() {
-        return car_fabrication_year;
-    }
-
-    public double getCar_battery_capacity() {
-        return car_battery_capacity;
-    }
-
-    public double getCar_max_range() {
-        return car_max_range;
-    }
-
     public void setIdentifier(int identifier) {
         this.identifier = identifier;
+    }
+
+    public String getCar_brand() {
+        return car_brand;
     }
 
     public void setCar_brand(String car_brand) {
         this.car_brand = car_brand;
     }
 
+    public String getCar_model() {
+        return car_model;
+    }
+
     public void setCar_model(String car_model) {
         this.car_model = car_model;
+    }
+
+    public int getCar_fabrication_year() {
+        return car_fabrication_year;
     }
 
     public void setCar_fabrication_year(int car_fabrication_year) {
         this.car_fabrication_year = car_fabrication_year;
     }
 
+    public double getCar_battery_capacity() {
+        return car_battery_capacity;
+    }
+
     public void setCar_battery_capacity(double car_battery_capacity) {
         this.car_battery_capacity = car_battery_capacity;
+    }
+
+    public double getCar_max_capacity() {
+        return car_max_capacity;
+    }
+
+    public void setCar_max_capacity(double car_battery_capacity) {
+        this.car_max_capacity = car_battery_capacity;
+    }
+
+    public double getCar_max_range() {
+        return car_max_range;
     }
 
     public void setCar_max_range(double car_max_range) {
         this.car_max_range = car_max_range;
     }
 
-    public void updateAutonomy(double distance) {
-        double newAutonomy = getCar_battery_capacity() - distance;
-        setCar_battery_capacity(newAutonomy);
+    public void updateBattery(double distance) {
+        double battery = getCar_battery_capacity() - distance;
+        setCar_battery_capacity(battery);
     }
 
-   public void recharge(double energy) {
-        this.car_battery_capacity = Math.min(this.car_battery_capacity + energy, car_max_capacity);
+    public void recharge() {
+        this.car_battery_capacity = this.car_max_capacity;
     }
 
     @Override
